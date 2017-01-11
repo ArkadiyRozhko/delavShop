dc53c07d30f53defea8facddcf5d739602e8a836
Ark <arkasha9@gmail.com>
2017-01-11 21:11:31 +0200

    add models class

---
+++ delavShop/Models/CreamContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace delavShop.Models
+{
+    public class CreamContext:DbContext
+    {
+        public  DbSet<Cream> Creams { get; set; }
+        public  DbSet<Purshase> Purshases { get; set; } 
+    }
+}
---
+++ delavShop/Models/Purshase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace delavShop.Models
+{
+    public class Purshase
+    {
+        public  int PurshaseId { get; set; }
+        public  string Person { get; set; }
+        public  int CreamId { get; set; }
+        public  string Address { get; set; }
+        public  DateTime Date { get; set; }
+    }
+}
