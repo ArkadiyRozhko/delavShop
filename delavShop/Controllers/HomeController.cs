4b82f71a8dad812a5a3e439982a2fb12f7c917d8
Аркадий <arkasha9@gmail.com>
2017-01-15 21:24:20 +0200

    Change Controller

--- delavShop/Controllers/HomeController.cs
+++ delavShop/Controllers/HomeController.cs
@@ -3,27 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using delavShop.Models;
 
 namespace delavShop.Controllers
 {
     public class HomeController : Controller
     {
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+
+        CreamContext db=new CreamContext();
+
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            IEnumerable<Cream> creams = db.Creams;
+            ViewBag.Creams = creams;
             return View();
         }
     }
