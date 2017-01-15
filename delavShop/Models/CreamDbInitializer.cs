606f3aed06fd851291b6043eba7832044fc3b6e0
Аркадий <arkasha9@gmail.com>
2017-01-15 22:04:28 +0200

    Добавив клас для заповнення товарами

---
+++ delavShop/Models/CreamDbInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace delavShop.Models
+{
+    public class CreamDbInitializer:DropCreateDatabaseAlways<CreamContext>
+    {
+        protected override void Seed(CreamContext db)
+        {
+            db.Creams.Add(new Cream {Name = "Для рук", Author = "Делав", Price = 150});
+            db.Creams.Add(new Cream {Name = "Для зняття макіяжу", Author = "Делав", Price = 300});
+            db.Creams.Add(new Cream {Name = "Гелеронка", Author = "Делав", Price = 500});
+            base.Seed(db);
+        }
+    }
+}
